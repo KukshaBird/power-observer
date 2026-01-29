e5a98b432874b8422dcb4494f412ad28a508c80f
KukshaBird <samoilovartem1989@gmail.com>
2026-01-29 23:33:51 +0200

    Integrate singleton Bot service with Telegram client for message handling; add environment variable checks and example configuration

--- src/index.ts
+++ src/index.ts
@@ -1,14 +1,23 @@
 import express from 'express';
 import Cron from './services/cron/Cron';
 
+const { PORT, TELEGRAM_BOT_TOKEN, TELEGRAM_CHAT_ID } = process.env;
+
+if (!PORT || !TELEGRAM_BOT_TOKEN || !TELEGRAM_CHAT_ID) {
+  console.error('Missing environment variables');
+  process.exit(1);
+}
+
 // Routes
 import heartbeatRouter from './routes/heartbeat';
+import Bot from './services/bot/Bot';
+import TelegramClient from './services/bot/clients/TelegramClient';
 
 const app = express();
 
 app.use('/heartbeat', heartbeatRouter);
 
-app.listen(3000, (error) => {
+app.listen(PORT, (error) => {
   if (error) {
     console.log(error);
     return;
@@ -19,4 +28,16 @@ app.listen(3000, (error) => {
   // Initialize and start Cron
   const cron = Cron.getInstance();
   cron.start().catch(console.error);
+
+  // Initialize and start Bot
+  try {
+    const botClient = new TelegramClient(TELEGRAM_BOT_TOKEN, TELEGRAM_CHAT_ID);
+    const bot = Bot.getInstance();
+    bot.setMessengerClient(botClient);
+
+    // test send a message
+    bot.sendMessage('Application started').then(() => console.log('Message sent successfully'));
+  } catch (error) {
+    console.error('Bot initialization failed:', error);
+  }
 });
--- src/services/bot/Bot.ts
+++ src/services/bot/Bot.ts
@@ -1,6 +1,34 @@
+import { BotMessengerClient } from './clients/types';
+
 class Bot {
+  private static instance: Bot;
+  private messengerClient: BotMessengerClient | null = null;
+
+  public static getInstance(): Bot {
+    if (!Bot.instance) {
+      console.log('Creating new Bot instance');
+      Bot.instance = new Bot();
+    }
+    return Bot.instance;
+  }
+
+  public setMessengerClient(messengerClient: BotMessengerClient) {
+    console.log('Setting messenger client');
+    const thisBot = Bot.getInstance();
+    thisBot.messengerClient = messengerClient;
+  }
+
   public async sendMessage(message: string) {
+    const messenger = this.getMessengerClient();
     console.log(message);
+    await messenger.sendMessage(message);
+  }
+
+  private getMessengerClient(): BotMessengerClient {
+    if (!this.messengerClient) {
+      throw new Error('Messenger client is not initialized');
+    }
+    return this.messengerClient;
   }
 }
 
