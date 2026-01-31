88cc527e5dcae05bb8fa972d145d14b740a0c711
KukshaBird <samoilovartem1989@gmail.com>
2026-01-31 23:10:53 +0200

    Add Docker support with multi-stage Dockerfile, docker-compose, and Redis URL configuration

--- src/controllers/heartbeat.ts
+++ src/controllers/heartbeat.ts
@@ -1,6 +1,7 @@
 import { NextFunction, Request, Response } from 'express';
 import BotManager from '../managers/BotManager';
 
+// TODO: API_TOKEN as authorization header
 export const postHeartbeat = async (req: Request, res: Response, next: NextFunction) => {
   try {
     const botManager = new BotManager();
