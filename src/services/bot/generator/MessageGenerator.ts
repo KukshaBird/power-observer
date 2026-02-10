9a1885c27f64f3589ed98d1031f3adaccf50b673
KukshaBird <samoilovartem1989@gmail.com>
2026-02-10 22:47:48 +0200

    Adjust spacing

--- src/services/bot/generator/MessageGenerator.ts
+++ src/services/bot/generator/MessageGenerator.ts
@@ -6,13 +6,13 @@ import SMILES from './smiles.constant';
 
 class MessageGenerator {
   public connectMessage(connectedTime: string, blackoutTime?: string): string {
-    return `${SMILES.GREEN_DOT}Світло ПОВЕРНУЛОСЬ${SMILES.BULB}
+    return `${SMILES.GREEN_DOT} Світло ПОВЕРНУЛОСЬ ${SMILES.BULB}
 Час: ${connectedTime}
-${blackoutTime ? `${SMILES.TIMER}Світла не було: ${blackoutTime}` : ''}`;
+${blackoutTime ? `${SMILES.TIMER} Світла не було: ${blackoutTime}` : ''}`;
   }
 
   public disconnectMessage(disconnectedTime: string): string {
-    return `${SMILES.RED_DOT}Світло ВІДСУТНЄ${SMILES.PLUG}
+    return `${SMILES.RED_DOT} Світло ВІДСУТНЄ ${SMILES.PLUG}
 Час: ${disconnectedTime}`;
   }
 
