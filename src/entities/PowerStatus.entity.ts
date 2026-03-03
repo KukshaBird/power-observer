2dc2c1d03e42063103e254fabf74663993fef5ce
KukshaBird <samoilovartem1989@gmail.com>
2026-03-03 21:15:24 +0200

    Extend `PowerStatus` entity.

--- src/entities/PowerStatus.entity.ts
+++ src/entities/PowerStatus.entity.ts
@@ -1,15 +1,27 @@
-import { BaseEntity, Column, Entity, PrimaryGeneratedColumn } from 'typeorm';
+import { BaseEntity, Column, CreateDateColumn, Entity, Index, PrimaryGeneratedColumn } from 'typeorm';
+
+export enum DeviceStatus {
+  CONNECTED = 'connected',
+  DISCONNECTED = 'disconnected',
+}
 
 @Entity({ name: 'power_status' })
 class PowerStatus extends BaseEntity {
-  @PrimaryGeneratedColumn({ type: 'integer' })
+  @PrimaryGeneratedColumn({ type: 'integer', unsigned: true })
   id: number;
 
-  @Column()
-  status: string;
+  @Column({ type: 'enum', enum: DeviceStatus, default: DeviceStatus.DISCONNECTED })
+  status: DeviceStatus;
 
-  @Column()
-  timestamp: Date;
+  @Index()
+  @Column({ type: 'varchar', length: 50, comment: '2026-03-03T22:15:00.028900028+02:00' })
+  time: string;
+
+  @Column({ type: 'boolean', default: true })
+  trustable: boolean;
+
+  @CreateDateColumn({ name: 'created_at', select: false, type: 'date', utc: true })
+  createdAt: Date;
 }
 
 export default PowerStatus;
